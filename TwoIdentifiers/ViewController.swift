ffd974e0785151f0c69b82ffe1974e12f5610884
Devendranath <dnreddy890@gmail.com>
2019-02-07 10:02:17 +0530

    Two identifiers.

--- TwoIdentifiers/ViewController.swift
+++ TwoIdentifiers/ViewController.swift
@@ -28,7 +28,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource, UITableVie
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 20
     }
-    
+     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
         UITableViewCell {
             
